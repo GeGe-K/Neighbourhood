d2a5c37b582789aaf43c445ed4004f4f9379620f
GeGe-K <gloriagivondo@gmail.com>
2019-01-14 02:28:13 +0300

    created class userprofile

--- watch/models.py
+++ watch/models.py
@@ -4,6 +4,8 @@ import datetime as dt
 
 # Create your models here.
 class Neighbourhood(models.Model):
+
+
     '''
     Neighbourhood class has the following properties
     '''
@@ -34,4 +36,26 @@ class Neighbourhood(models.Model):
         self.occupants +=1
         self.save()
 
-   
+class UserProfile(models.Model):
+    '''
+    UserProfile class has the following properties
+    '''
+
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20,blank=True)
+    email = models.CharField(max_length = 40, blank = True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey('Neighborhood', on_delete=models.CASCADE, null=True, blank=True)
+
+    def assign_neighborhood(self, neighborhood):
+        self.neighborhood = neighborhood
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.user.username}'
