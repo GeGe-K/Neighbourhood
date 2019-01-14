8288fb6d5f7b2086f271b0635090f54d1beac7e1
GeGe-K <gloriagivondo@gmail.com>
2019-01-14 04:12:28 +0300

    configured the admin

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighbourhood, User, Business, EmergencyContacts, Post
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(UserProfile)
+dmin.site.register(Business)
+admin.site.register(EmergencyContacts)
+admin.site.register(Post)
