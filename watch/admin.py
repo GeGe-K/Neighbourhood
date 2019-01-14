f05132b5e86e002a5b9b3099eb0a2c68d13f04af
GeGe-K <gloriagivondo@gmail.com>
2019-01-14 04:41:51 +0300

    configured the welcome email

--- watch/admin.py
+++ watch/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Neighbourhood, User, Business, EmergencyContacts, Post
+from .models import Neighbourhood, UserProfile, Business, EmergencyContacts, Post
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(UserProfile)
-dmin.site.register(Business)
+admin.site.register(Business)
 admin.site.register(EmergencyContacts)
 admin.site.register(Post)
