64fa87ecddffa501d5f0f5f84b9fa2580792436f
GeGe-K <gloriagivondo@gmail.com>
2019-01-16 16:11:41 +0300

    updated the urls

--- watch/urls.py
+++ watch/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^signout/$', views.signout, name='signout'),
     url(r'^neighbourhood/(\d+)', views.neighbourhood, name='neighbourhood'),
     url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^update_profile/(\d+)$', views.update_profile, name='update_profile'),
     url(r'^search/', views.search, name='search'),
     url(r'^add_business/', views.add_business, name='add_business'),
     url(r'^change_neighbourhood/(\d+)', views.change_neighbourhood, name='change_neighbourhood'),
